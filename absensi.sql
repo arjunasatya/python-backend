b1b1bf2d7f8fb57309e19e74c8119728bafe40a8
arjunasatya <arjunasatya53@gmail.com>
2025-02-04 16:36:54 +0700

    buat user pada database.py dan passwordmya sekalian

--- absensi.sql
+++ absensi.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 14, 2025 at 04:32 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Feb 04, 2025 at 09:22 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `username` varchar(50) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`username`, `nama`, `date`, `time`) VALUES
+('juna', 'juna', '2025-01-31', '00:18:04'),
+('juna', 'arjunasatya', '2025-01-31', '00:39:15'),
+('juna', 'arjunasatya', '2025-01-31', '01:22:23'),
+('juna', 'arjunasatya', '2025-01-31', '01:38:38'),
+('aldi', 'aldichan', '2025-02-01', '16:37:23'),
+('juna', 'arjunasatya', '2025-02-02', '12:35:33'),
+('aldi', 'aldichan', '2025-02-02', '12:38:17');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login_user`
 --
 
@@ -31,18 +57,44 @@ CREATE TABLE `login_user` (
   `nama` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password` varchar(100) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `login_user`
+--
+
+INSERT INTO `login_user` (`nama`, `email`, `username`, `password`, `role`) VALUES
+('aldichan', 'aldi@gmail.com', 'aldi', 'aldi123', 'user'),
+('devansyah', 'devan@gmail.com', 'devan', 'devan123', 'user'),
+('arjunasatya', 'arjuna@gmail.com', 'juna', 'juna123', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD KEY `username` (`username`);
+
+--
 -- Indexes for table `login_user`
 --
 ALTER TABLE `login_user`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`username`) REFERENCES `login_user` (`username`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 db = mysql.connector.connect(
     host='127.0.0.1',
-    user='root',
-    password='',
+    user='flask_user',
+    password='password123',
     database='absensi'
 )
